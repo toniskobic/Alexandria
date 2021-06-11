93b939264f4f42d1f2de87c2716d44bfb08969d9
piljeg <80128125+piljeg@users.noreply.github.com>
2021-06-11 19:21:45 +0200

    Kostur projekta - troslojna arhitektura

--- Software/Alexandria/BussinessLayer/Form1.Designer.cs
+++ Software/Alexandria/BussinessLayer/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Alexandria
+namespace BussinessLayer
 {
     partial class Form1
     {
--- Software/Alexandria/BussinessLayer/Properties/Resources.Designer.cs
+++ Software/Alexandria/BussinessLayer/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace Alexandria.Properties
+namespace BussinessLayer.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace Alexandria.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Alexandria.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BussinessLayer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
