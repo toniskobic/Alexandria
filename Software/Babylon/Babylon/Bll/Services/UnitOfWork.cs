bd292b9938c007e5a36ed0ddd1dee8fcf1cc823c
piljeg <80128125+piljeg@users.noreply.github.com>
2021-06-14 01:28:19 +0200

    PI project: -completed UnitOfWork

--- Software/Babylon/Babylon/Bll/Services/UnitOfWork.cs
+++ Software/Babylon/Babylon/Bll/Services/UnitOfWork.cs
@@ -12,11 +12,35 @@ namespace Bll.Services
         protected AppDbContext _appDbContext { get; set; }
         public UserRepository Users { get; set; }
         public PickingInRepository PickingIns { get; set; }
+        public AuthorRepository Authors { get; set; }
+        public CategoryRepository Categories { get; set; }
+        public LiteratureRepository Literatures { get; set; }
+        public LoanRepository Loans { get; set; }
+        public LoanItemRepository LoanItems { get; set; }
+        public MembershipRepository Memberships { get; set; }
+        public PickingInItemRepository PickingInItems { get; set; }
+        public PickingOutRepository PickingOuts { get; set; }
+        public PickingOutItemRepository PickingOutItems { get; set; }
+        public ReceiptRepository Receipts { get; set; }
+        public ReceiptItemRepository ReceiptItems { get; set; }
+        public RoleRepository Roles { get; set; }
 
         public UnitOfWork(AppDbContext dbContext)
         {
             Users = new UserRepository(_appDbContext);
             PickingIns = new PickingInRepository(_appDbContext);
+            Authors = new AuthorRepository(_appDbContext);
+            Categories = new CategoryRepository(_appDbContext);
+            Literatures = new LiteratureRepository(_appDbContext);
+            Loans = new LoanRepository(_appDbContext);
+            LoanItems = new LoanItemRepository(_appDbContext);
+            Memberships = new MembershipRepository(_appDbContext);
+            PickingInItems = new PickingInItemRepository(_appDbContext);
+            PickingOuts = new PickingOutRepository(_appDbContext);
+            PickingOutItems = new PickingOutItemRepository(_appDbContext);
+            Receipts = new ReceiptRepository(_appDbContext);
+            ReceiptItems = new ReceiptItemRepository(_appDbContext);
+            Roles = new RoleRepository(_appDbContext);
             _appDbContext = dbContext;
         }
 
