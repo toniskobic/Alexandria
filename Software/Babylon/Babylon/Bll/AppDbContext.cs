b2d2ca3a20ce6d3933a03f2a1a309f7933d81dca
Luka Bojka <bojka.luka@gmail.com>
2021-06-13 18:33:20 +0200

    Azuriran AppDbContext

--- Software/Babylon/Babylon/Bll/AppDbContext.cs
+++ Software/Babylon/Babylon/Bll/AppDbContext.cs
@@ -18,6 +18,18 @@ namespace Bll
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<ReceiptItem> ReceiptItems { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<LoanItem> LoanItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Arrival> Arrivals { get; set; }
+        public DbSet<ArrivalItem> ArrivalItems { get; set; }
+        public DbSet<Dismiss> Dismisses { get; set; }
+        public DbSet<DismissItem> DismissItems { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
