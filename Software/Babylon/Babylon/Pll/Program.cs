fe433076458f9afc64bd309e9459ac9e12edebcd
TONIPC\Toni <tskobic@foi.hr>
2021-06-15 05:38:33 +0200

    PI project: -minor changes made

--- Software/Babylon/Babylon/Pll/Program.cs
+++ Software/Babylon/Babylon/Pll/Program.cs
@@ -18,9 +18,7 @@ namespace Pll
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLoans());
-            //Application.Run(new FormLateLoansList());
-
+            Application.Run(new FormLogin());
         }
     }
 }
